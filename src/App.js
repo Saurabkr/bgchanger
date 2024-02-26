609355b4e2ce85883103f4278233d90d2c2fda1f
Saurabkr <82012354+Saurabkr@users.noreply.github.com>
2024-02-26 22:49:05 +0530

    Project: Bg-Changer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("olive");
+  let arr = ['red', 'green', 'blue', 'purple', 'yellow', 'brown', 'pink', 'orange', 'cyan'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="w-full h-screen duration-200" style={{ backgroundColor:color }}>
+      <div className="fixed flex flex-wrap justify-center container bottom-12 px-2">
+      <div className="flex flex-wrap justify-center gap-5 shadow-lg bg-white px-2 py-3 rounded-3xl">
+         {arr.map((color, index) => (
+          
+          <button key={index} onClick={() => setColor(color)} className='outline-none px-4 rounded-lg' style={{ backgroundColor: color }}>
+
+           {color.charAt(0).toUpperCase() + color.slice(1)}
+          </button>
+         ))}
+      </div>
+            
+      </div>
+     </div>
+    </>
   );
 }
 
